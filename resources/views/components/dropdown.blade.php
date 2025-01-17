10c11375b7d83d8a39c046174463e3d88a86c7e3
kilina irina p-42 <gisi@bk.ru>
2025-01-17 21:49:46 +0500

    Сделала голубую обводку для выпадающего меню в header.

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700 border-[3px] rounded-lg border-sky-300'])
 
 @php
 $alignmentClasses = match ($align) {
